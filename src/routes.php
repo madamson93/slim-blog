6eef2ffe53e8ef1f869c8f69d81d8b8d38e030f2
Monique Adamson <madamson@hotmail.co.uk>
2017-10-15 22:28:09 +0100

    Make changes to routes
    Retrieve form data from the POST route (route used when form is submitted)

--- src/routes.php
+++ src/routes.php
@@ -7,12 +7,17 @@ use Slim\Http\Response;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $request->getAttribute('name');
     
-	// Render template
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->get('/name-meaning', function (Request $request, Response $response) {
+$app->get('/name-meaning/', function (Request $request, Response $response) {
     
-	// Render template
     return $this->renderer->render($response, 'name-form.phtml');
+});
+
+$app->post('/name-meaning/submit', function ($request, $response, $args) {
+	$firstName = $request->getParam('formFirstName');
+    $lastName = $request->getParam('formLastName');
+   	
+   	return $this->renderer->render($response, 'name-results.phtml', $args);
 });
